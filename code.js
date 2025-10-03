c721bd306ccef84bd9091b122be730dff63893d2
thcheng54 <thcheng54@gapps.cityu.edu.hk>
2025-10-03 17:10:30 +0800

    In-Class Participation 2

--- code.js
+++ code.js
@@ -1,2 +1,10 @@
 var title = document.querySelector("h1");
 title.innerHTML = "This is the title from code.js";
+
+var button = document.querySelector("button");
+
+button.addEventListener("click", myfunction);
+
+function myfunction(){
+    alert("Let me tell you more about myself!")
+}
