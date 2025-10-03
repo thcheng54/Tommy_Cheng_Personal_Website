c9b403bc555121497f6aacb42f94d52f279de43f
thcheng54 <thcheng54@gapps.cityu.edu.hk>
2025-10-03 17:51:06 +0800

    In-Class Participation 2

--- code.js
+++ code.js
@@ -1,5 +1,5 @@
-var title = document.querySelector("h1");
-title.innerHTML = "This is the title from code.js";
+//var title = document.querySelector("h1");
+//title.innerHTML = "This is the title from code.js";
 
 var button = document.querySelector("button");
 
@@ -7,4 +7,16 @@ button.addEventListener("click", myfunction);
 
 function myfunction(){
     alert("Let me tell you more about myself!")
+}
+
+var mynode = document.createElement("div");
+mynode.id = "work1_intro";
+mynode.innerHTML = "The work is an exhibition";
+mynode.style.color = "blue";
+
+mynode.addEventListener("click", welcomeToWork1);
+document.querySelector("#my_work1").appendChild(mynode);
+
+function welcomeToWork1() {
+    mynode.innerHTML = "Thank you for your interest in my work!"
 }
